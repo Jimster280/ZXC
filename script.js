500681373cc15ee37210073945d0a67509686899
Jimster280 <razumov_feodor@mail.ru>
2021-11-25 20:23:57 +0300

    добавил переменные алёрт и консоль лог

---
+++ script.js
@@ -0,0 +1,8 @@
+var title = 10;
+var screens = 256;
+var screenPrice = 1280;
+var rollback = 5;
+var fullPrice = 2560;
+var adaptive = 300;
+console.log("ZXC гуль");
+alert("Блинк Ветра Ульта Z X пауза тычка C пауза");
