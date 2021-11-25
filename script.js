a7b8a896a486462fa23b51f66be267a8a8a62fd1
Jimster280 <razumov_feodor@mail.ru>
2021-11-25 20:57:28 +0300

    убрал знаки равенства

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-let title = ;
-let screens = ;
-let screenPrice = ;
-let rollback = ;
-let fullPrice = ;
-let adaptive = ;
+let title;
+let screens;
+let screenPrice;
+let rollback;
+let fullPrice;
+let adaptive;
 console.log("ZXC гуль");
 alert("Блинк Ветра Ульта Z X пауза тычка C пауза");
