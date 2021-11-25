0c7cb59790f34899a48cc56ff6cd0b770289f867
Jimster280 <razumov_feodor@mail.ru>
2021-11-25 20:49:40 +0300

    поменял var на let

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-var title = ;
-var screens = ;
-var screenPrice = ;
-var rollback = ;
-var fullPrice = ;
-var adaptive = ;
+let title = ;
+let screens = ;
+let screenPrice = ;
+let rollback = ;
+let fullPrice = ;
+let adaptive = ;
 console.log("ZXC гуль");
 alert("Блинк Ветра Ульта Z X пауза тычка C пауза");
