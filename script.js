eaf495cd127a97798c8f67ebba5713ad12285d26
Jimster280 <razumov_feodor@mail.ru>
2021-11-26 16:39:10 +0300

    выполнил все задания урока 2

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+let title = "ZXC";
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 500;
+let rollback = 57;
+let fullPrice = 2000;
+let adaptive = true;
 console.log("ZXC гуль");
-alert("Блинк Ветра Ульта Z X пауза тычка C пауза");
+// alert("Блинк Ветра Ульта Z X пауза тычка C пауза");
+
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screens.length);
+console.log(
+  "Стоимость верстки экранов" +
+    "  " +
+    screenPrice +
+    "  " +
+    "рублей" +
+    "  " +
+    "Стоимость разработки сайта" +
+    "  " +
+    fullPrice +
+    "  " +
+    "рублей"
+);
+
+var newScreens = screens.toLowerCase();
+
+console.log(newScreens.split());
+
+console.log(fullPrice * (rollback / 100));
