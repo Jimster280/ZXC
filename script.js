119eff7ff7d797576b7bcf80114631e3c29c85b5
Jimster280 <razumov_feodor@mail.ru>
2021-11-29 21:44:46 +0300

    feature: выполнил задание

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
-const title = "ZXC";
-const screens = "Простые, Сложные, Интерактивные";
-const screenPrice = 500;
+const title = prompt("Как называется ваш проект?");
+const screens = prompt(
+  "Какие типы экранов нужно разработать?",
+  "(пример: Простые, Сложные, Интерактивные"
+);
+const screenPrice = prompt(
+  "Сколько будет стоить данная работа?",
+  "пример: 12000"
+);
 const rollback = 57;
-const fullPrice = 2000;
-const adaptive = true;
+const adaptive = prompt("Нужен ли адаптив на сайте?");
+
+let service1 = prompt("Какой дополнительный тип услуги нужен?");
+
+let servicePrice1 = prompt("Сколько это будет стоить");
+
+let service2 = prompt("Какой дополнительный тип услуги нужен?");
+
+let servicePrice2 = prompt("Сколько это будет стоить");
+
+const fullPrice = screenPrice + servicePrice1 + servicePrice2;
+13;
+
+let servicePercentPrice = fullPrice - 1000;
+
 console.log("ZXC гуль");
 // alert("Блинк Ветра Ульта Z X пауза тычка C пауза");
 
@@ -28,3 +47,13 @@ console.log(
 console.log(screens.toLowerCase().split());
 
 console.log(fullPrice * (rollback / 100));
+
+console.log(Math.round(servicePercentPrice));
+
+if ((fullPrice) => 30000) {
+  console.log("Даем скидку в 10%");
+} else if ((fullPrice) => 15000 && fullPrice < 30000) {
+  console.log("Даем скидку в 5%");
+} else if (fullPrice > 0 && fullPrice < 15000) {
+  console.log("Скидка не предусмотрена");
+}
